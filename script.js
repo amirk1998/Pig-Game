71324ffc1d180ea212f8cb44fecc005bb7329196
amirk1998 <amirk1998.pv@gmail.com>
2021-08-11 19:38:22 +0430

    Winning Score was fixed

--- script.js
+++ script.js
@@ -116,7 +116,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     //2.Check if Player's score is >=100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       //Finish the game
       playing = false;
       diceEl.classList.add('hidden');
